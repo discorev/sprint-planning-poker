55f412c47ed3da2d3047e2080f855f1932e81446
Ollie Hayman <discorev@olliespage.net>
2020-12-14 22:49:25 +0000

    Prevent the user-interface updating once choice has been locked

--- frontend/src/app/cards/cards.component.ts
+++ frontend/src/app/cards/cards.component.ts
@@ -9,9 +9,13 @@ export class CardsComponent {
   cards = ['?', '1', '2', '3', '5', '8', '13', '21'];
 
   @Input() selection?: string;
+  @Input() disabled = false;
   @Output() selectionChange = new EventEmitter<string>();
 
   choose(option: string): void {
+    if (this.disabled) {
+      return;
+    }
     if (this.selection === option) {
       this.selection = undefined;
     } else {
