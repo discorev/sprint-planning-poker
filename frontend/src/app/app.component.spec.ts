8b1ba7b117d11cb49572a0deed24b9954514a3bf
Ollie Hayman <discorev@olliespage.net>
2021-01-07 07:50:03 +0000

    Started to add tests mocking the webservice

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -10,11 +10,14 @@ describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let webSocketServiceSpy: jasmine.SpyObj<WebSocketService>;
-  const openSubject$ = new Subject();
-  const closeSubject$ = new Subject();
-  const onMessage$ = new Subject();
+  let openSubject$: Subject<unknown>;
+  let closeSubject$: Subject<unknown>;
+  let onMessage$: Subject<unknown>;
 
   beforeEach(async () => {
+    openSubject$ = new Subject();
+    closeSubject$ = new Subject();
+    onMessage$ = new Subject();
     const spy = jasmine.createSpyObj('WebSocketService', ['send'], {
       openSubject$, closeSubject$, onMessage$
     });
@@ -38,6 +41,12 @@ describe('AppComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    closeSubject$.complete();
+    openSubject$.complete();
+    onMessage$.complete();
+  });
+
   it('should create the app', () => {
     expect(app).toBeTruthy();
   });
@@ -47,19 +56,26 @@ describe('AppComponent', () => {
     fixture.whenStable().then(() => {
       const modalBdEl: HTMLElement = fixture.debugElement.query(By.css('.modal-backdrop')).nativeElement;
       expect(modalBdEl).toBeTruthy();
+      closeSubject$.complete();
     });
 
   });
 
   it('should hide the connecting modal when the open subject is called', () => {
-    openSubject$.next('');
+    closeSubject$.next('');
     fixture.whenStable().then(() => {
-      const modalBdDebugEl: DebugElement = fixture.debugElement.query(By.css('.modal-backdrop'));
-      expect(modalBdDebugEl).toBeNull();
+      const modalBdEl: HTMLElement = fixture.debugElement.query(By.css('.modal-backdrop')).nativeElement;
+      expect(modalBdEl).toBeTruthy();
+      openSubject$.next('');
+
+      fixture.whenStable().then(() => {
+        const modalBdDebugEl: DebugElement = fixture.debugElement.query(By.css('.modal-backdrop'));
+        expect(modalBdDebugEl).toBeNull();
+      });
     });
   });
 
-  it('should send a selection to the WebSocket', () => {
+  it('should send selection to the WebSocket when it is changed', () => {
     const sendSpy = webSocketServiceSpy.send;
     app.selectionChanged('?');
     fixture.detectChanges();
@@ -67,4 +83,46 @@ describe('AppComponent', () => {
     const expected: any = {choice: '?', action: 'record-choice'};
     expect(sendSpy.calls.first().args[0]).toEqual(expected);
   });
+
+  it('should send reset event to the WebSocket when sendRest is called', () => {
+    const sendSpy = webSocketServiceSpy.send;
+    app.sendReset();
+    fixture.detectChanges();
+    expect(sendSpy.calls.count()).toBe(1);
+    const expected: any = {action: 'reset'};
+    expect(sendSpy.calls.first().args[0]).toEqual(expected);
+  });
+
+  it('should accept a list of players from the WebSocket', () => {
+    const players = ['player1', 'player2', 'player3'];
+    const expected = players.map(player => {
+      return { name: player, selected: false};
+    });
+    onMessage$.subscribe(_ => expect(app.players).toEqual(expected));
+    onMessage$.next({ players });
+  });
+
+  it('should update a players status when they make a selection', () => {
+    app.players = [
+      { name: 'player1', selected: false },
+      { name: 'player2', selected: false },
+      { name: 'player3', selected: false }
+    ];
+
+    const expected = [
+      { name: 'player1', selected: false },
+      { name: 'player2', selected: true },
+      { name: 'player3', selected: false }
+    ];
+    onMessage$.subscribe(_ => expect(app.players).toEqual(expected));
+    onMessage$.next({ name: 'player2', selected: true });
+  });
+
+  // it('should add a player to the list if they are not already there and make a choice', () => {
+  //   const expected = [
+  //     { name: 'player2', selected: true }
+  //   ];
+  //   onMessage$.subscribe(_ => expect(app.players).toEqual(expected));
+  //   onMessage$.next({ name: 'player2', selected: true });
+  // });
 });
