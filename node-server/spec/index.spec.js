8121204296a67f757e48f7f1c1f17d3d3935a11e
Ollie Hayman <discorev@olliespage.net>
2021-01-10 19:15:03 +0000

    remove superwstest

--- node-server/spec/index.spec.js
+++ node-server/spec/index.spec.js
@@ -1,44 +1,85 @@
 const WebSocket = require('ws');
-const request = require('superwstest');
 
-describe("WebSocket Server", function() {
-    var wss;
+// hack to make unit tests block without causing the server to stop
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
 
-    beforeEach(function(done) {
-      delete require.cache[require.resolve('../index')];
-      wss = require('../index');
-      wss.on('listening', () => {
-        console.log('Listening on port 3000');
+describe("WebSocket Server", function () {
+  var webSocketServer;
+
+  beforeEach(function (done) {
+    // ensure the websocket restarts by deleting the previous import cache
+    delete require.cache[require.resolve('../index')];
+    webSocketServer = require('../index');
+    webSocketServer.on('listening', () => {
+      console.log('Listening on port 3000');
+      done();
+    });
+  });
+
+  afterEach(function (done) {
+    if (webSocketServer != null) {
+      sleep(100);
+      webSocketServer.close(function close() {
+        webSocketServer = null;
         done();
       });
+    }
+  });
+
+  it("Test the websocket is connected", function() {
+    const ws = new WebSocket('ws://localhost:3000');
+    ws.on('open', function () {
+      expect(ws.readyState).toBe(1);
+      ws.close();
+    });
+    sleep(5);
+  });
+
+  it("should reject a single character name", function() {
+    const ws = new WebSocket('ws://localhost:3000');
+    ws.on('message', function incoming(data) {
+      expect(data).toBe('{"error": "name is too short"}');
+      ws.close();
     });
 
-    afterEach(function(done) {
-      wss.close(done);
-    });
-  
-    it("Test the websocket is connected", function(done) {
-      const ws = new WebSocket('ws://localhost:3000');
-      ws.on('open', function() {
-        expect(ws.readyState).toBe(1);
-        done();
-      });
+    ws.on('open', () => {
+      ws.send('{"action": "register", "name", "a"');
     });
 
-    it("should reject registering names shorter than 3 characters", async () => {
-      await request
-            .ws('/')
-            .sendJson({action: "register", name: "a"})
-            .expectJson({error: "name is too short"});
+    sleep(5);
+  });
 
-      // const ws = new WebSocket('ws://localhost:3000');
-      // ws.on('message', function incoming(data) {
-      //   expect(data).toBe('{"error": "name is too short"}');
-      //   done();
-      // });
-
-      // ws.on('open', () => {
-      //   ws.send('{"action": "register", "name", "a"');
-      // });
+  it("should reject a two character name", function() {
+    const ws = new WebSocket('ws://localhost:3000');
+    ws.on('message', function incoming(data) {
+      expect(data).toBe('{"error": "name is too short"}');
+      ws.close();
     });
+
+    ws.on('open', () => {
+      ws.send('{"action": "register", "name", "aa"');
+    });
+
+    sleep(5);
+  });
+
+  it("should register a three character name", function() {
+    const ws = new WebSocket('ws://localhost:3000');
+    ws.on('message', function incoming(data) {
+      expect(JSON.parse(data)).toBe({error: null, players: ['aaa'], reset: true});
+      ws.close();
+    });
+
+    ws.on('open', () => {
+      ws.send('{"action": "register", "name", "aaa"');
+    });
+
+    sleep(5);
+  });
 });
