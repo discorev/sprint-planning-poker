325150629a96e2f268aadd88721001299984051c
Ollie Hayman <discorev@olliespage.net>
2021-01-25 17:33:00 +0000

    fix: docker now limits pulls so login should be before build

--- node-server/scripts/deploy.sh
+++ node-server/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VERSION=`node -p "require('./package').version"`
-docker build -t discorev/sprint-planning-poker-server:latest -t discorev/sprint-planning-poker-server:$VERSION .
+# login first to ensure pulls are done against my limit not the travis limit
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t discorev/sprint-planning-poker-server:latest -t discorev/sprint-planning-poker-server:$VERSION .
 docker push discorev/sprint-planning-poker-server:latest
 docker push discorev/sprint-planning-poker-server:$VERSION
