a2d58f7f77b38012c9c87ba7f01079d47de32284
Ollie Hayman <discorev@olliespage.net>
2021-01-27 17:14:35 +0000

    replace legacy backticked notation

--- node-server/scripts/deploy.sh
+++ node-server/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=`node -p "require('./package').version"`
+VERSION=$(node -p "require('./package').version")
 # login first to ensure pulls are done against my limit not the travis limit
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t discorev/sprint-planning-poker-server:latest -t discorev/sprint-planning-poker-server:$VERSION .
